899ab183c74dad62b37fe67745b56c9f3e973ff5
Felipe Augusto Machado Sampaio Miguel <felipeasmiguel@gmail.com>
2023-08-14 09:08:53 -0300

    Operadores aritméticos, lógicos e relacionais.

--- lista031/questao02.py
+++ lista031/questao02.py
@@ -1,14 +1,14 @@
 '''
-Elaborar um programa que pergunte quatro valores e apresente 2 resultados:
+Elaborar um programa que pergunte quatro valores inteiros e apresente 2 resultados:
 a) O resultado de suas adições
 b) O resultado de suas multiplicações
 '''
 
 #Input
-num1 = float(input("Informe o primeiro valor"))
-num2 = float(input("Informe o segundo valor"))
-num3 = float(input("Informe o terceiro valor"))
-num4 = float(input("Informe o quarto valor"))
+num1 = int(input("Informe o primeiro valor"))
+num2 = int(input("Informe o segundo valor"))
+num3 = int(input("Informe o terceiro valor"))
+num4 = int(input("Informe o quarto valor"))
 
 #Cálculo
 soma = num1 + num2 + num3 + num4
